4df11079c0e171edbc4b3bf8f2941f81e4710b8f
980327 <alap_u@naver.com>
2022-05-09 00:48:40 +0900

    버블정렬

--- 알고리즘/src/BubbleSort.java
+++ 알고리즘/src/BubbleSort.java
@@ -9,7 +9,7 @@ public class BubbleSort {
 
     // 선택정렬 알고리즘
     public static void bubbleSort(int[] arr) {
-        for (int i = 0; i < arr.length-1; i++) {
+        for (int i = 0; i < arr.length; i++) {
             for (int j = 1; j < arr.length-i; j++) {
                 if (arr[j-1] > arr[j]) {
                     int temp = arr[j-1];
