fc9e62e11593fd836815885eb3c72a2d56466cfb
980327 <alap_u@naver.com>
2022-05-12 12:50:01 +0900

    선택정렬 - 수정

--- algorithm/src/SelectionSort.java
+++ algorithm/src/SelectionSort.java
@@ -12,7 +12,7 @@
         for (int i = 0; i < arr.length; i++) {
             int min = i;
             for (int j = i+1; j < arr.length; j++) {
-                if (arr[i] > arr[j]) {
+                if (arr[min] > arr[j]) {
                     min = j;
                 }
             }
